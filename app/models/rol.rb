acd627d9894c6e59deb5d4e517be37ee32a5c614
avasquezb <alberto.vasquezb@outlook.com>
2016-09-23 19:18:00 -0300

    lista la base

--- app/models/rol.rb
+++ app/models/rol.rb
@@ -1,3 +1,4 @@
 class Rol < ActiveRecord::Base
 	self.table_name = "roles"
+	has_many :usuarios
 end
--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,5 +1,6 @@
 class Usuario < ActiveRecord::Base
 	has_many :historials
+	belongs_to :rol
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
