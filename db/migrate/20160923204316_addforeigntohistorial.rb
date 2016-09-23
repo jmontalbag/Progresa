b25fe439e8caeea0d3d62bdd2db320eef920fccb
Jose Moltalba <jose.montalba@hotmail.com>
2016-09-23 18:56:38 -0300

    actualizando llaves foraneas de historial

--- db/migrate/20160923204316_addforeigntohistorial.rb
+++
@@ -1,8 +0,0 @@
-class Addforeigntohistorial < ActiveRecord::Migration
-  def change
-  	add_foreign_key :historials, :usuarios, :column: :usuario_id, primary_key: "id"
-  	add_foreign_key :historials, :beneficiarios, :column: :beneficiario_id, primary_key: "id"
-  	add_foreign_key :historials, :estados, :column: :estado_id, primary_key: "id"
-  	add_foreign_key :historials, :objetivos, :column: :objetivos_id, primary_key: "id"
-  end
-end
