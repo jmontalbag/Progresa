bfe4fb7e075e8e66ce5395d01e37bfd3a706f5b7
avasquezb <alberto.vasquezb@outlook.com>
2016-09-23 18:28:06 -0300

    avanzando

---
+++ db/migrate/20160923204316_addforeigntohistorial.rb
@@ -0,0 +1,8 @@
+class Addforeigntohistorial < ActiveRecord::Migration
+  def change
+  	add_foreign_key :historials, :usuarios, :column: :usuario_id, primary_key: "id"
+  	add_foreign_key :historials, :beneficiarios, :column: :beneficiario_id, primary_key: "id"
+  	add_foreign_key :historials, :estados, :column: :estado_id, primary_key: "id"
+  	add_foreign_key :historials, :objetivos, :column: :objetivos_id, primary_key: "id"
+  end
+end
