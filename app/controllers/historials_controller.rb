ea880bedc0cdf6459c679699bbd422dab411663e
Jose Moltalba <jose.montalba@hotmail.com>
2016-09-30 12:12:37 -0300

    Trabajo en login

--- app/controllers/historials_controller.rb
+++ app/controllers/historials_controller.rb
@@ -1,8 +1,12 @@
 class HistorialsController < ApplicationController
   def index
   	@historials = Historial.paginate(:page => params[:page], :per_page => 10).order('id ASC')
-  	@estado = Estado.select("nombre").where(:estado_id => params[:id]);
+  	@estado = Estado.select("Nombre").where(:estado_id => params[:id]);
   end
   def editar
+  end
+
+  def beneficiario_params
+    params.require(:historial).permit(:usuario_id, :beneficiario_id, :estado_id, :objetivos_id)
   end
 end
